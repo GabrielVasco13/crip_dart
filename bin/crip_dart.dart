5e13c141392010b98a315e7b55242dd101e98cb1
Gabriel FV <gabrielvasco906@gmail.com>
2025-04-17 16:13:59 -0300

    Implement encryption and decryption functionality in main method

--- bin/crip_dart.dart
+++ bin/crip_dart.dart
@@ -1,1 +1,15 @@
-void main() {}
+import 'package:encrypt/encrypt.dart';
+
+void main() {
+  final plainText = 'Testando';
+  final key = Key.fromUtf8('my 32 length key................');
+  final iv = IV.fromLength(16);
+
+  final encrypter = Encrypter(AES(key));
+
+  final encrypted = encrypter.encrypt(plainText, iv: iv);
+  final decrypted = encrypter.decrypt(encrypted, iv: iv);
+
+  print(decrypted); // Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  print(encrypted.base64);
+}
